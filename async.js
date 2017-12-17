8c54868b4a0e6f8e3429a57e8bc34dbb7e9fe0a2
momo <418788724@qq.com>
2017-12-17 23:08:20 +0800

    init project

--- async.js
+++ async.js
@@ -126,5 +126,33 @@
         }
         next();
     }
+
     runAuto(gen);
+
+})();
+
+/**
+ * ES7中async异步执行，Generator函数的语法糖
+ */
+;
+(() => {
+
+    const fs = require('fs');
+
+    const readFile = function (fileName) {
+        return new Promise(function (resolve, reject) {
+            fs.readFile(fileName, function (error, data) {
+                if (error) return reject(error);
+                resolve(data);
+            });
+        });
+    };
+
+    const asyncReadFile = async function () {
+        const f1 = await readFile('input.txt');
+        const f2 = await readFile('output.txt');
+        console.log(f1.toString());
+        console.log(f2.toString());
+    };
+    asyncReadFile();
 })();
